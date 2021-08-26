e7a468aee58ba2345881d6211a30019fdec43424
Rakibur Khan <therakiburkhan@gmail.com>
2021-08-26 17:33:03 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rakibur Khan on 26/Aug/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
