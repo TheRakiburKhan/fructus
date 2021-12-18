df28c657d42e024105cb388b4cd6285823ea6d7e
Rakibur Khan <therakiburkhan@gmail.com>
2021-12-19 03:54:14 +0600

    Color Extension Added

---
+++ Fructus/Extension/Color.swift
@@ -0,0 +1,63 @@
+//
+//  Color.swift
+//  Fructus
+//
+//  Created by Rakibur Khan on 18/Dec/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    public static let appleDark: Color = Color("ColorAppleDark")
+    
+    public static let appleLight: Color = Color("ColorAppleLight")
+    
+    public static let blueberryDark: Color = Color("ColorBlueberryDark")
+    
+    public static let blueberryLight: Color = Color("ColorBlueberryLight")
+    
+    public static let cherryDark: Color = Color("ColorCherryDark")
+    
+    public static let cherryLight: Color = Color("ColorCherryLight")
+    
+    public static let gooseberryDark: Color = Color("ColorGooseberryDark")
+    
+    public static let gooseberryLight: Color = Color("ColorGooseberryLight")
+    
+    public static let grapefruitDark: Color = Color("ColorGrapefruitDark")
+   
+    public static let grapefruitLight: Color = Color("ColorGrapefruitLight")
+    
+    public static let lemonDark: Color = Color("ColorLemonDark")
+   
+    public static let lemonLight: Color = Color("ColorLemonLight")
+    
+    public static let limeDark: Color = Color("ColorLimeDark")
+    
+    public static let limeLight: Color = Color("ColorLimeLight")
+    
+    public static let mangoDark: Color = Color("ColorMangoDark")
+    
+    public static let mangoLight: Color = Color("ColorMangoLight")
+   
+    public static let pearDark: Color = Color("ColorPearDark")
+   
+    public static let pearLight: Color = Color("ColorPearLight")
+   
+    public static let plumDark: Color = Color("ColorPlumDark")
+    
+    public static let plumLight: Color = Color("ColorPlumLight")
+   
+    public static let pomegranateDark: Color = Color("ColorPomegranateDark")
+    
+    public static let pomegranateLight: Color = Color("ColorPomegranateLight")
+   
+    public static let strawberryDark: Color = Color("ColorStrawberryDark")
+   
+    public static let strawberryLight: Color = Color("ColorStrawberryLight")
+    
+    public static let watermelonDark: Color = Color("ColorWatermelonDark")
+  
+    public static let watermelonLight: Color = Color("ColorWatermelonLight")
+}
