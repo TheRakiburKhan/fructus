c9fe9294fc0b1a38ba1919a56f21152836c57129
Rakibur Khan <therakiburkhan@gmail.com>
2021-12-19 03:57:12 +0600

    Onboarding view created

---
+++ Fructus/View/OnboardingView.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Rakibur Khan on 19/Dec/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: Properties
+    
+    //MARK: Body
+    var body: some View {
+        TabView {
+            ForEach(fruitsData, id:\.self.id) {fruit in
+                FruitCardView(fruit: fruit)
+            } //Loop
+        } //TabView
+        .tabViewStyle(.page)
+        .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
